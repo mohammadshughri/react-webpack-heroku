1fe73b44a58959855b7f7401c03f053de15224cc
mohammadshughri <mohammadws19@gmail.com>
2022-12-16 15:27:36 +0300

    Clean up and added packages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                Hello World !
-            </div>
-        )
-    }
-}
-ReactDOM.render(<App />, document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+window.React = React;
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
